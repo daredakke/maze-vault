eaac183e94a3b992b48deead6b3b1e61a2375e32
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-10 16:05:34 +0000

    Forcefields that only robots can pass through

---
+++ scripts/forcefield.gd
@@ -0,0 +1,26 @@
+class_name Forcefield
+extends Node2D
+
+
+var _wait_time: int = 2
+var _wait_count: int = 0
+
+@onready var stood_on_ray: RayCast2D = $StoodOnRay
+
+
+func _process(_delta: float) -> void:
+	_wait_count += 1
+	
+	if _wait_count >= _wait_time:
+		_check_if_stood_on()
+		_wait_count = 0
+
+
+func _check_if_stood_on() -> void:
+	stood_on_ray.force_raycast_update()
+	
+	var collider := stood_on_ray.get_collider()
+	
+	if collider is Player:
+		collider.destroy()
+		EventBus.level_reset.emit()
