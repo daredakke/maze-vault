030978404d201bd5a455248a8435cc2810d0addb
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-12 10:54:03 +0000

    Level 8

--- scripts/level.gd
+++ scripts/level.gd
@@ -9,9 +9,11 @@ const LEVEL_04: PackedScene = preload("res://scenes/levels/level_04.tscn")
 const LEVEL_05: PackedScene = preload("res://scenes/levels/level_05.tscn")
 const LEVEL_06: PackedScene = preload("res://scenes/levels/level_06.tscn")
 const LEVEL_07: PackedScene = preload("res://scenes/levels/level_07.tscn")
+const LEVEL_08: PackedScene = preload("res://scenes/levels/level_08.tscn")
 
 
 var _level_scenes: Array = [
+	LEVEL_08,
 	LEVEL_07,
 	LEVEL_06,
 	LEVEL_02,
