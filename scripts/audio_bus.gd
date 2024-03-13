5a10b5fadf6e73ba9239972eb48a224616f9688f
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-13 07:35:08 +0000

    Audio bus and SFX

---
+++ scripts/audio_bus.gd
@@ -0,0 +1,94 @@
+class_name AudioBus
+extends Node
+
+
+@onready var player_death_sfx: AudioStreamPlayer = $PlayerDeathSFX
+@onready var robot_death_sfx: AudioStreamPlayer = $RobotDeathSFX
+@onready var box_move_sfx: AudioStreamPlayer = $BoxMoveSFX
+@onready var menu_hover_sfx: AudioStreamPlayer = $MenuHoverSFX
+@onready var menu_select_sfx: AudioStreamPlayer = $MenuSelectSFX
+@onready var level_reset_sfx: AudioStreamPlayer = $LevelResetSFX
+@onready var teleporter_activate_sfx: AudioStreamPlayer = $TeleporterActivateSFX
+@onready var teleporter_deactivate_sfx: AudioStreamPlayer = $TeleporterDeactivateSFX
+@onready var teleport_sfx: AudioStreamPlayer = $TeleportSFX
+@onready var shot_fired_sfx: AudioStreamPlayer = $ShotFiredSFX
+@onready var robot_released_sfx: AudioStreamPlayer = $RobotReleasedSFX
+@onready var button_pressed_sfx: AudioStreamPlayer = $ButtonPressedSFX
+@onready var button_released_sfx: AudioStreamPlayer = $ButtonReleasedSFX
+
+
+func play_menu_selected() -> void:
+	menu_select_sfx.play()
+
+
+func play_menu_hovered() -> void:
+	if not menu_select_sfx.playing:
+		menu_hover_sfx.play()
+
+
+func play_player_death() -> void:
+	player_death_sfx.play()
+
+
+func play_robot_death() -> void:
+	robot_death_sfx.play()
+
+
+func play_box_move() -> void:
+	box_move_sfx.play()
+
+
+func play_level_reset() -> void:
+	level_reset_sfx.play()
+
+
+func play_teleporter_activated() -> void:
+	teleporter_activate_sfx.play()
+
+
+func play_teleporter_deactivated() -> void:
+	teleporter_deactivate_sfx.play()
+
+
+func play_teleported() -> void:
+	teleport_sfx.play()
+
+
+func play_shot_fired() -> void:
+	shot_fired_sfx.play()
+
+
+func play_robot_released() -> void:
+	robot_released_sfx.play()
+
+
+func play_button_pressed() -> void:
+	button_pressed_sfx.play()
+
+
+func play_button_released() -> void:
+	button_released_sfx.play()
+
+
+func _on_player_death_sfx_finished() -> void:
+	_modulate_pitch(player_death_sfx)
+
+
+func _on_robot_death_sfx_finished() -> void:
+	_modulate_pitch(robot_death_sfx)
+
+
+func _on_shot_fired_sfx_finished() -> void:
+	_modulate_pitch(shot_fired_sfx)
+
+
+func _on_robot_released_sfx_finished() -> void:
+	_modulate_pitch(robot_released_sfx)
+
+
+func _on_box_move_sfx_finished() -> void:
+	_modulate_pitch(box_move_sfx)
+
+
+func _modulate_pitch(audio_player: AudioStreamPlayer) -> void:
+	audio_player.pitch_scale = randf_range(0.93, 1.07)
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,14 @@
 class_name Game
 extends Node2D
 
-
+@onready var audio_bus: Node = $AudioBus
 @onready var splash: Splash = %Splash
 @onready var pause: Pause = %Pause
 @onready var level: Level = %Level
+
+
+func _enter_tree() -> void:
+	get_tree().node_added.connect(_connect_button_sfx)
 
 
 func _ready() -> void:
@@ -32,6 +36,18 @@ func _process(_delta: float) -> void:
 		level.next_level()
 
 
+func _connect_button_sfx(node: Node) -> void:
+	if node is Button:
+		node.mouse_entered.connect(_menu_hovered)
+		node.pressed.connect(_menu_selected)
+	
+	if node is HSlider:
+		node.mouse_entered.connect(_menu_hovered)
+		
+	if node.is_in_group("sfx_slider"):
+		node.drag_ended.connect(_sfx_slider_drag_ended)
+
+
 func _start_game() -> void:
 	Global.game_started = true
 	
@@ -56,3 +72,15 @@ func _handle_pause_state() -> void:
 			pause.focus_main_menu()
 	else:
 		pause.hide()
+
+
+func _menu_hovered() -> void:
+	audio_bus.play_menu_hovered()
+
+
+func _menu_selected() -> void:
+	audio_bus.play_menu_selected()
+
+
+func _sfx_slider_drag_ended(_value_changed: float) -> void:
+	audio_bus.play_menu_selected()
